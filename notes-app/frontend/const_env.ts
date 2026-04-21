589ea0de3666e920970c413256f7405059ed78bc
rayan-rt <tanzeemrayan2@gmail.com>
2026-04-21 16:20:07 +0500

    Test and Code quality coverage has been setup via vitest and sonar

--- notes-app/frontend/const_env.ts
+++ notes-app/frontend/const_env.ts
@@ -1,5 +1,6 @@
 const ENV_VARS: Record<string, string> = {
   BACKEND_URL: String(import.meta?.env?.VITE_BACKEND_URL),
+  BACKEND_API_VERSION: String(import.meta?.env?.VITE_BACKEND_API_VERSION),
 } as const;
 
 export { ENV_VARS };
