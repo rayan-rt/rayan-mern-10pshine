c36b33bf9bf636d03080f19d0a03774954dc699c
rayan-rt <tanzeemrayan2@gmail.com>
2026-04-21 13:43:57 +0500

    feat(frontend): implement logger with remote transport and env config

--- notes-app/frontend/src/App.tsx
+++ notes-app/frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { logger } from "./utils/logger.js";
 
 function App() {
+  React.useEffect(() => {
+    logger.info("App component mounted");
+    logger.error("Test remote log from Frontend!");
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-semibold text-blue-500">
